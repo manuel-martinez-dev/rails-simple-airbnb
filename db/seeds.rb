8ad10565aea005badaa9adc40436d6d11c8f24c1
manuel martinez <manuel.martinez59000@gmail.com>
2019-11-16 13:06:40 +0100

    added photo urls

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ puts 'Creating Flats...'
 Flat.create!(
   name: 'Light & Spacious Garden Flat London',
   address: '10 Clifton Gardens London W9 1DT',
-  picture_url:'',
+  picture_url:'https://images.unsplash.com/photo-1499916078039-922301b0eb9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80',
   description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
   price_per_night: 75,
   number_of_guests: 3
@@ -22,7 +22,7 @@ Flat.create!(
 Flat.create!(
   name: 'Hellish hell hole',
   address: '4 Hell road, Hell',
-  picture_url: '',
+  picture_url: 'https://images.unsplash.com/photo-1541051595295-e7d4ae8b845f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
   description: 'Full of fire acid and suffering, the perfect place to enjoy pain',
   price_per_night: 100,
   number_of_guests: 2
@@ -31,7 +31,7 @@ Flat.create!(
 Flat.create!(
   name: 'Heaven on earth',
   address: '10 peaceful street, Earth',
-  picture_url:'',
+  picture_url:'https://images.unsplash.com/photo-1440407876336-62333a6f010f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1353&q=80',
   description: 'A heavenly place for you to rest for all eternity',
   price_per_night: 110_000_58,
   number_of_guests: 1
@@ -40,7 +40,7 @@ Flat.create!(
 Flat.create!(
   name: 'Feline manor',
   address: '52 B Tom Sawyer road, Catland',
-  picture_url:'',
+  picture_url:'https://images.unsplash.com/photo-1565190462842-d57a3e05de48?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=664&q=80',
   description: 'Just another place where cats rule and you are their pawn',
   price_per_night: 60,
   number_of_guests: 2
@@ -49,7 +49,7 @@ Flat.create!(
 Flat.create!(
   name: 'Confined and super small flat',
   address: '10 shittiest road, crap city',
-  picture_url:'',
+  picture_url:'https://images.unsplash.com/photo-1464890100898-a385f744067f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
   description: 'if you got little money and are in need of place to crash, look no further',
   price_per_night: 10,
   number_of_guests: 4
